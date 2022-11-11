7212ffb13c3d09ca1e2d6cf7f194ba415c404fb9
gzheng0214 <gzheng0214@gmail.com>
2022-11-10 21:19:52 -0500

    added register route

---
+++ serverless/member/handler.py
@@ -0,0 +1,23 @@
+import boto3
+import json
+
+def main(event, context):
+    try:
+        print(f"Triggered event: {event}")
+        resource = boto3.resource('dynamodb')
+        membersTable = resource.Table('Members')
+        body = json.loads(event["body"])
+        print(f"Inserting into table: {body}")
+        membersTable.put_item(Item={
+            **body, 
+            "id": body["timestamp"] + "|" + body["email"],
+            "prior_matches": [],
+            "round": {}
+            }
+        )
+        response = {"statusCode": 200, "body": "Success!"}
+    except Exception as e:
+        response = {"statusCode": 400, "body": f"Something went wrong! Check it out: {e}" }
+    return response
+
+
