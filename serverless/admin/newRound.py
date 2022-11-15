9127b6be87400ae4593447f6f961de19969a05bc
Yang Minghui <minghuiyang1998@gmail.com>
2022-11-14 21:49:52 -0500

    complete new round test on server

--- serverless/admin/newRound.py
+++ serverless/admin/newRound.py
@@ -7,6 +7,7 @@ roundsTable = client.Table('Rounds')
 
 def write_to_rounds(next):
     # { round_number: number; groups: {}} 
+    print('write_to_rounds')
     roundsTable.put_item(Item={
         'round_number': next,
         'groups': {}
@@ -14,14 +15,17 @@ def write_to_rounds(next):
     
 def write_to_member(next):
     # for each member {  }
+    print('write_to_member')
     members = memberTable.scan() 
     items = members['Items'] 
     for m in items:
         round = m['round']
+        # print(m)
         round[next] = {
             'opted_in': False, 
             'group': -1
         }
+        # print(round)
         memberTable.update_item(
             Key={'id': m['id']},
             UpdateExpression='SET round = :val',
@@ -34,31 +38,36 @@ def lambda_handler(event, context):
     # read all keys in the Rounds table. and sort
     rounds = roundsTable.scan()
     items = rounds['Items']
+    
     lastKey = 0
     if items:
-        items.sort(key=items['round_number'], reverse=True)
+        def get_round_numb(round):
+            return int(round.get('round_number'))
+        items.sort(key=get_round_numb, reverse=True)
         lastKey = items[0]['round_number']
     # if empty then we know it’s round 1 else lastkey + 1 
-    next = lastKey + 1
+    next = str(lastKey + 1)
+    print(next)
 
-    # 2.  add a empty object in table rounds
     try:
+        # 2.  add a empty object in table rounds
         write_to_rounds(next)
-    except Exception:
-        return {
-            'statusCode': 500,
-            'body': Exception
-        }
-    # 3. read all member and add round
-    try:
+        # 3. read all member and add round
         write_to_member(next)
     except Exception:
         return {
-            'statusCode': 500,
-            'body': Exception
+            "statusCode": 500, 
+            "body": json.dumps({
+                "statusCode": 500,
+                "message": f" wrong on server: {Exception}"
+            })
         }
         
     # succeed return 
     return {
-        'statusCode': 200
-    } 
+        "statusCode": 200, 
+        "body": json.dumps({
+            "statusCode": 200,
+            "message": "Success!"
+        })
+    }
