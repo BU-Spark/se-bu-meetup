1ae6674aacbf9c891b2fcfb757c62f0f1e0e3c86
gzheng0214 <gzheng0214@gmail.com>
2022-11-15 19:45:25 -0500

    reformat register file

--- serverless/member/register.py
+++ serverless/member/register.py
@@ -5,28 +5,34 @@ import logging
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+
 def main(event, context):
     try:
         logger.info(f"Triggered event: {event}")
-        resource = boto3.resource('dynamodb')
-        membersTable = resource.Table('Member')
+        resource = boto3.resource("dynamodb")
+        membersTable = resource.Table("Member")
         body = json.loads(event["body"])
         item = {
-            **body, 
+            **body,
             "id": body["timestamp"] + "|" + body["email"],
             "prior_matches": [],
-            "round": {}
+            "round": {},
         }
         logger.info(f"Inserting into table: {item}")
         membersTable.put_item(Item=item)
-        response = {"statusCode": 200, "body": json.dumps({
+        response = {
             "statusCode": 200,
-            "message": "Success!"
-        })}
+            "body": json.dumps({"statusCode": 200, "message": "Success!"}),
+        }
     except Exception as e:
         logger.error(f"Exception: {e}")
-        response = {"statusCode": 400, "body": json.dumps({
+        response = {
             "statusCode": 400,
-            "message": f"Something went wrong! Check it out: {e}"
-        }) }
+            "body": json.dumps(
+                {
+                    "statusCode": 400,
+                    "message": f"Something went wrong! Check it out: {e}",
+                }
+            ),
+        }
     return response
