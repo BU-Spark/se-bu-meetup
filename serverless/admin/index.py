334cb7230e3fe1925ad5bd3db849efe609c9669d
Yang Minghui <minghuiyang1998@gmail.com>
2022-11-17 17:30:16 -0500

    create and deploy home page

---
+++ serverless/admin/index.py
@@ -0,0 +1,11 @@
+def lambda_handler(event, context):
+    f = open("template/index.html", "r");
+    html = f.read()
+    
+    return {
+        "statusCode": 200,
+        "body": html.replace("\n", ""),
+        "headers": {
+            "Content-Type": "text/html"
+        }
+    }
